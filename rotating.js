b258deffa5a7471ebaefeedfd0360371ab119146
C0rine <Corine_J@MSN.com>
2015-11-05 02:02:00 +0100

    added counter clockw rot.button and implemted guess button

--- rotating.js
+++ rotating.js
@@ -6,7 +6,7 @@
 var theimage = document.getElementById('theimage');
 var deg = 0;
 
-function makeRotation(){
+function makeRotationCW(){
 	if (deg == 0 || deg == 90 || deg == 180 || deg == 270){
 		deg += 90;
 		console.log("add 90 --" + deg);
@@ -21,4 +21,30 @@ function makeRotation(){
 	}
 
 	theimage.style.WebkitTransform = "rotate("+ deg + "deg)";
+}
+
+function makeRotationCCW(){
+	if (deg == 0 || deg == 90 || deg == 180 || deg == 270){
+		deg -= 90;
+		if (deg == -90){
+			deg = 270;
+		}
+		console.log("subtract 90 --" + deg);
+	}
+	else{
+		console.log("Something went wrong in the calculation of the rotation degree. This should never happen.")
+	}
+
+	theimage.style.WebkitTransform = "rotate("+ deg + "deg)";
+}
+
+function makeGuess(){
+	if (deg == 0){
+		// the user was right
+		window.alert("You were right!");
+	}
+	else{
+		// the user was wrong
+		window.alert("You were wrong :( ...");
+	}
 }
