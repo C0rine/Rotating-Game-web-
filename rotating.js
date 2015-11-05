9e3d5d7f5676a826bb16c3a9b7919623f7f4ad1b
C0rine <Corine_J@MSN.com>
2015-11-05 01:45:21 +0100

    finished assignment 1, implemented js to rotate image

--- rotating.js
+++ rotating.js
@@ -1,1 +1,24 @@
-window.alert("hello world!");
+/* Corine Jacobs
+   10001326
+   Universiteit van Amsterdam
+   Corine_J@MSN.com */
+
+var theimage = document.getElementById('theimage');
+var deg = 0;
+
+function makeRotation(){
+	if (deg == 0 || deg == 90 || deg == 180 || deg == 270){
+		deg += 90;
+		console.log("add 90 --" + deg);
+	}
+	else{
+		console.log("Something went wrong in the calculation of the rotation degree. This should never happen.")
+	}
+
+	if (deg == 360){
+		deg = 0;
+		console.log("back to 0  --" + deg);
+	}
+
+	theimage.style.WebkitTransform = "rotate("+ deg + "deg)";
+}
